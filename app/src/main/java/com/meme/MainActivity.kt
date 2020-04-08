f0f099e83e74e0b1bc6ae2a6415a1917b53a11c1
Pavel Mikrasov <mikrasov.p@gmail.com>
2020-04-08 18:31:48 +0300

    Background color and surf logo were added to basic main activity

--- app/src/main/java/com/meme/MainActivity.kt
+++ app/src/main/java/com/meme/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.meme
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.annotation.IdRes
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
 }
