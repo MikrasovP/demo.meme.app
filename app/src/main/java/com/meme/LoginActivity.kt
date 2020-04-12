dedbd2cb8eaecf8e9a8cf9de18dd2f48a785ac1a
Pavel Mikrasov <mikrasov.p@gmail.com>
2020-04-12 11:16:27 +0300

    Login button with ripple and sprinner were added

--- app/src/main/java/com/meme/LoginActivity.kt
+++ app/src/main/java/com/meme/LoginActivity.kt
@@ -2,10 +2,18 @@ package com.meme
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 
 class LoginActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun onLoginButtonClicked(view: View){
+        view.visibility = View.INVISIBLE
+        val bar:ProgressBar = findViewById(R.id.loginProgressBar)
+        bar.visibility = View.VISIBLE
     }
 }
