3fb130a989e05ca092b087827177ae691a9c3f57
Pavel Mikrasov <mikrasov.p@gmail.com>
2020-04-26 19:15:25 +0300

    Activity layout was fully done

--- app/src/main/java/com/meme/ui/main/adder/AdderActivity.kt
+++ app/src/main/java/com/meme/ui/main/adder/AdderActivity.kt
@@ -9,5 +9,6 @@ class AdderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adder)
+        setSupportActionBar(findViewById(R.id.adder_toolbar))
     }
 }
